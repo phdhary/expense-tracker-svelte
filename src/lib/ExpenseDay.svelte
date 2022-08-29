139a65f62d44791ada796b52c5c637ab0ff18f2d
Hary Prihadi <phdhary@gmail.com>
2022-08-29 21:56:26 +0700

    add expanse day component

---
+++ src/lib/ExpenseDay.svelte
@@ -0,0 +1,25 @@
+<script>
+	export /**
+	 * @type {any}
+	 */
+	let title;
+</script>
+
+<div class="expense-day">{title}</div>
+<hr />
+
+<style>
+	hr {
+		width: 700px;
+		border: 0.5px solid #dedede;
+	}
+	.expense-day {
+		margin-top: 20px;
+		display: flex;
+		justify-items: start;
+		flex-direction: row;
+		text-align: start;
+		font-size: 18px;
+		font-weight: bold;
+	}
+</style>
