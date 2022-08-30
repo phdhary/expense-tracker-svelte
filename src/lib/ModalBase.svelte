3710387c251ddcdf0535bff48747330ea677507b
Hary Prihadi <phdhary@gmail.com>
2022-08-30 17:28:31 +0700

    create ModalBase and ModalButton

---
+++ src/lib/ModalBase.svelte
@@ -0,0 +1,90 @@
+<script>
+	/**
+	 * @type {HTMLDivElement}
+	 */
+	var modal;
+	/**
+	 * @type {any}
+	 */
+	export let child;
+
+	export function closeModal() {
+		modal.style.display = 'none';
+	}
+	export function showModal() {
+		modal.style.display = 'block';
+	}
+</script>
+
+<div bind:this={modal} class="modal">
+	<div class="modal-content">
+		<span on:click={closeModal} class="close">&times;</span>
+		<svelte:component this={child} />
+        <slot name="child"></slot>
+	</div>
+</div>
+
+<style>
+	.modal {
+		display: none;
+		position: fixed;
+		z-index: 1;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		background-color: rgb(0, 0, 0);
+		background-color: rgba(0, 0, 0, 0.4);
+		animation-name: fadeIn;
+		animation-duration: 0.4s;
+	}
+
+	.modal-content {
+		background-color: #fefefe;
+		/* margin: 5% auto; */
+		padding: 20px;
+		border: 1px solid #888;
+		width: var(--modalWidth);
+		bottom: 20%;
+		position: fixed;
+		left: 32%;
+		animation-name: animateFromBottom;
+		animation-duration: 0.4s;
+		border-radius: 15px;
+	}
+
+	.close {
+		color: #aaa;
+		float: right;
+		font-size: 28px;
+		font-weight: bold;
+	}
+
+	.close:hover,
+	.close:focus {
+		color: black;
+		text-decoration: none;
+		cursor: pointer;
+	}
+	/* Add Animation */
+	@keyframes animateFromBottom {
+		from {
+			bottom: -500px;
+			opacity: 0;
+		}
+		to {
+			bottom: 20%;
+			opacity: 1;
+		}
+	}
+
+	@keyframes fadeIn {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+</style>
---
+++ src/lib/ModalButton.svelte
@@ -0,0 +1,17 @@
+<script>
+	import ModalBase from './ModalBase.svelte';
+	/**
+	 * @type {ModalBase}
+	 */
+	let modalBase;
+	export let name = 'Please name this button';
+	/**
+	 * @type {any}
+	 */
+	export let child;
+	export let modalWidth = 28;
+</script>
+
+<button class="black-button" on:click={() => modalBase.showModal()}>{name}</button>
+
+<ModalBase bind:this={modalBase} {child} --modalWidth="{modalWidth}%" />
